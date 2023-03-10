b7b934db766682c59968cfcac2464ea05dedccfc
Дарья Кротова <daru42ru@gmail.com>
2023-03-10 10:53:53 +0700

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int result;
+        if (price > 10000) {
+            result = 500;
+        } else {
+            result = price / 20;
+        }
+        return result;
+    }
+}
